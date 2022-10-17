ff9a544939a6b97992ef88a71cfd00efd1a3be1f
Kenji <moodzombie@gmail.com>
2022-10-17 08:18:29 -0300

    feat: change routes and api to ts

--- kenzie_hub/src/routes/router.tsx
+++ kenzie_hub/src/routes/router.tsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
-import Login from "@pages/Login";
-import Register from "@pages/SignUp";
-import Dashboard from "@pages/Dashbord";
 import ProtectedRoutes from "@components/ProtectedRoutes";
+import Login from "@pages/Login";
+import SignUp from "@pages/SignUp";
+import Dashboard from "@pages/Dashboard";
 
 export default () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/signup" element={<Register />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route element={<ProtectedRoutes />}>
         <Route path="/dashboard" element={<Dashboard />} />
       </Route>
